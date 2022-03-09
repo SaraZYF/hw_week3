804ba2df36ea890cbed40ba6bf00f0f453c70cba
SaraZYF <60347364+SaraZYF@users.noreply.github.com>
2022-03-09 20:02:42 +1100

    updated generated password function

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 //Code added from here//
 //promot choice password Criteria, Input collection//
 //length//
+//window.confirm can be used as blooean option//
 var number=window.prompt ('Y or N for number in password');
 //upperCase, true or false//
 var upperCase=window.prompt ('Y or N for upperCase in password');
@@ -21,7 +14,7 @@ var lowerCase=window.prompt ('Y or N for lowercase in password');
 //symbol, true or false//
 var symbol=window.prompt ('Y or N for symbol in password, for example !@#');
 //length, 8 to 125 characters//
-var length=window.prompt ('enter ideal length of password between 8 to 125 characters');
+var length=window.prompt ('enter ideal length of password between 8 to 125 characters, number only');
 
 
 console.log(number,upperCase,lowerCase,symbol,length);
@@ -74,7 +67,7 @@ return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
 
 //part3: function password and check length//
 function generatePassword() {
-var passwordBox=document.getElementById("password");
+var passwordText=document.getElementById("password");
 let password="";
 while (length.value > password.length) {
  let keyToAdd = getKey [Math.floor(Math.random()*getKeySymbol.length)];
@@ -84,9 +77,17 @@ while (length.value > password.length) {
  }
 }
 
-passwordBox.innerHTML = password;
+passwordText.value = password;
 }
 
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
 
 
 
@@ -94,4 +95,4 @@ passwordBox.innerHTML = password;
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
